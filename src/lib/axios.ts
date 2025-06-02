b7486644d18988a7fc54aeb4374b3784798ffcea
luisvanegra <luisvanegra@gmail.com>
2025-06-02 15:25:50 +0200

    solucion nuevo backend

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://wallet-flow-control-production.up.railway.app',
+  baseURL: import.meta.env.VITE_API_URL || 'https://wallet-flow-control2-production.up.railway.app',
   headers: {
     'Content-Type': 'application/json'
   }
